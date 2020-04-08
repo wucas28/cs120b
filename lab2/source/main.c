40cde7e8ea79211efd6754b8b8220363223f22d0
Lucas Wang <lwang215@wch136-13.cs.ucr.edu>
2020-04-07 23:30:14 -0700

    xin zhao win now

---
+++ lab2/source/main.c
@@ -0,0 +1,26 @@
+/*	Author: lwang215
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+DDRA = 0x00; PORTA = 0xFF;
+DDRB = 0xFF; PORTB = 0x00;
+
+unsigned char tempB= 0x00;
+    while (1) {
+	tempB=0x00;
+	tempB=~PINA;
+	PORTB = tempB;	
+    }
+    return 1;
+}
